ae978edd247f10d145aae794a82685a59a8fe389
Batuhan Oelmez <batuhan.oelmez@gambit.de>
2022-06-08 08:37:48 +0000

    Init

---
+++ orders/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("gambit.demo.orders.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
